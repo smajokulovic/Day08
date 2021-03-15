4a52f53ba7794a95bca8f891ca4a790852db1844
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-15 12:00:58 +0100

    Functions

---
+++ ex01/src/index.js
@@ -0,0 +1,12 @@
+function reusableFunction() {
+
+    reusableFunction = ("Hi Padawan!");
+
+    // Write code below this line
+    // Write code above this line
+}
+console.log("Hi Padawan!")
+module.exports = reusableFunction;
+
+
+
